2d48b443a4797e7a20e98a8fc647420d4152bfce
vzyteam-create <vzyteam@gmail.com>
2026-02-10 16:08:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,7 +102,7 @@ class LeaderboardStates(StatesGroup):
     viewing = State()
 
 # ========== ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ ==========
-bot = Bot(token=BOT_TOKEN)  # просто без parse_mode
+bot = Bot(token=BOT_TOKEN, parse_mode="Markdown")  # parse_mode
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 router = Router()
@@ -2471,4 +2471,5 @@ async def periodic_tasks():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
